6de121f6d5441599b54d12afc7b8a183e2cfbd1e
George Arthur <Arthurgeorgekojo@gmail.com>
2025-04-28 08:52:15 +0000

    Implemented serverless deployment with added lambda function

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { deleteFile, listFiles, uploadFile } from './controllers/file.controller
 
 dotenv.config();
 
-const app = express();
+export const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -3,9 +3,9 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-    region: process.env.AWS_REGION!
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID_L!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_L!,
+    region: process.env.AWS_REGION_L!
 })
 
 export const s3 = new AWS.S3();
